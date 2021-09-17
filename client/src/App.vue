c8f6cc5cdb6765e9fe430ff97e95652e6089051c
이세라 <you@example.com>
2021-09-17 16:22:50 +0900

    [S05P21A204-98] Design: 구글 폰트 및 아이콘 CDN 추가

--- client/src/App.vue
+++ client/src/App.vue
@@ -4,7 +4,8 @@
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Noto Sans KR', sans-serif;
+  /* font-family: 'Montserrat', sans-serif; */
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
 }
