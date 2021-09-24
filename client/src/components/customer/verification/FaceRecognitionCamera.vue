0b1a52406b0d9d45ee7aa4a7bfa59d7ee2af8368
ssafy-mann <6513032@gmail.com>
2021-09-24 15:26:32 +0900

    [S05P21A204-183] Feat: 얼굴인식 페이지 진입 시 카메라 구동 구현 및 확인

---
+++ client/src/components/customer/verification/FaceRecognitionCamera.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="f-column">
+    <div class="camera-header">
+      <span class="t-white fw-700 header-text">FACE 인식</span>
+    </div>
+    <video class="camera-stream" ref="camera" autoplay></video>
+    <canvas class="canvas-face"></canvas>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'FaceRecognitionCamera',
+  components: {
+  },
+  props: {},
+  data() {
+    return {}
+  },
+  created() {
+    this.createCameraElement()
+  },
+  methods: {
+    createCameraElement() {
+      const constraints = (window.constraints = {
+				audio: false,
+				video: true
+			})
+      navigator.mediaDevices
+				.getUserMedia(constraints)
+				.then(stream => {
+					this.$refs.camera.srcObject = stream;
+				})
+				.catch(error => {
+          console.log(error)
+					alert("지원하지 않는 브라우저입니다.");
+				});
+    }
+  }
+}
+</script>
--- client/src/views/customer/verification/FaceRecognition.vue
+++ client/src/views/customer/verification/FaceRecognition.vue
@@ -1,18 +1,21 @@
 <template>
   <div class="face-recognition">
+    <FaceRecognitionCamera/>
   </div>
 </template>
 
 <script>
 import './verification.scss'
+import FaceRecognitionCamera from '@/components/customer/verification/FaceRecognitionCamera.vue'
 
 export default {
   name: 'FaceRecognition',
   components: {
+    FaceRecognitionCamera
   },
   props: {},
   data() {
-    return {}  
+    return {}
   }
 }
 </script>
