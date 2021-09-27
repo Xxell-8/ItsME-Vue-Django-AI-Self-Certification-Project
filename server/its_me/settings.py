fa9e4666c2db65847fbcfb4d41ee543fc3653631
kiung <rldnd22@gmail.com>
2021-09-27 12:19:02 +0900

    Fix: default authentication classes 수정

--- server/its_me/settings.py
+++ server/its_me/settings.py
@@ -156,8 +156,6 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.SessionAuthentication',
-        'rest_framework.authentication.BasicAuthentication',
         'dj_rest_auth.jwt_auth.JWTCookieAuthentication',
     ],
 }
