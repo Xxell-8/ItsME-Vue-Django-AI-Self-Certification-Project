17f636c9d9874bc73090b6d7793dbe30b3b3a92e
kyinl <kyinle2@gmail.com>
2021-09-27 14:23:43 +0900

    [S05P21A204-186] Fix: 401 error

--- server/its_me/settings.py
+++ server/its_me/settings.py
@@ -155,9 +155,6 @@ REST_FRAMEWORK = {
     ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,
-    'DEFAULT_AUTHENTICATION_CLASSES': [
-        'dj_rest_auth.jwt_auth.JWTCookieAuthentication',
-    ],
 }
 
 SITE_ID = 1
