7f125b1464337141bbdc9f6e2222d362d361ea1b
ssafy-mann <6513032@gmail.com>
2021-09-27 11:25:16 +0900

    [S05P21A204-183] Feat: 얼굴인식 결과를 보여주는 canvas 구현

--- client/src/components/customer/verification/FaceRecognitionCamera.vue
+++ client/src/components/customer/verification/FaceRecognitionCamera.vue
@@ -1,14 +1,22 @@
 <template>
   <div class="f-column">
+    <!-- header -->
     <div class="camera-header">
-      <span class="t-white fw-700 header-text">FACE 인식</span>
+      <span class="t-white fw-700 header-text">Face 인식</span>
     </div>
-    <video class="camera-stream" ref="camera" autoplay></video>
-    <canvas class="canvas-face"></canvas>
+    <!-- camera -->
+    <video @loadeddata="initDetector" class="camera-stream" ref="camera" autoplay></video>
+    <!-- canvas -->
+    <canvas class="canvas-face" ref="canvas"></canvas>
+    <!-- buttons -->
+    <button class="t-white">사진찍기</button>
   </div>
 </template>
 
 <script>
+import '@tensorflow/tfjs-core';
+import '@tensorflow/tfjs-backend-webgl';
+import * as blazeface from '@tensorflow-models/blazeface';
 
 export default {
   name: 'FaceRecognitionCamera',
@@ -16,16 +24,19 @@ export default {
   },
   props: {},
   data() {
-    return {}
+    return {
+      model: null,
+      photoTaken: false,
+    }
   },
-  created() {
-    this.createCameraElement()
+  mounted() {
+    this.createCameraElement();
   },
   methods: {
     createCameraElement() {
       const constraints = (window.constraints = {
 				audio: false,
-				video: true
+				video: { width: 320, height: 240}
 			})
       navigator.mediaDevices
 				.getUserMedia(constraints)
@@ -36,7 +47,44 @@ export default {
           console.log(error)
 					alert("지원하지 않는 브라우저입니다.");
 				});
+    },
+    stopCameraStream() {
+      const tracks = this.$refs.camera.srcObject.getTracks()
+      tracks.forEach(track => {
+        track.stop()
+      })
+    },
+    async detectFaces() 
+    {
+      const camera = this.$refs.camera
+      const canvas = this.$refs.canvas
+      const ctx = canvas.getContext("2d")
+      const prediction = await this.model.estimateFaces(camera, false)
+      
+      ctx.canvas.width = 320
+      ctx.canvas.height = 240
+      ctx.drawImage(camera, 0, 0, 320, 240)
+      /* 얼굴 주변 사각형 그리기 */
+      prediction.forEach((pred) => {
+        ctx.beginPath();
+        ctx.lineWidth = "4";
+        ctx.strokeStyle = "blue";
+        ctx.rect(
+          pred.topLeft[0],
+          pred.topLeft[1],
+          pred.bottomRight[0] - pred.topLeft[0],
+          pred.bottomRight[1] - pred.topLeft[1]
+        );
+        ctx.stroke();        
+      });
+    },
+    async initDetector() {
+      // Vue3 문제 해결: Object.freeze
+      this.model = Object.freeze(await blazeface.load());
+      if (!this.photoTaken) {
+        setInterval(this.detectFaces, 100)
+      }
     }
-  }
+  }  
 }
 </script>
--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,6 +1,7 @@
 import { createStore } from 'vuex'
 import partner from './modules/partner'
 import customer from './modules/customer'
+import accounts from './modules/accounts'
 import createPersistedState from "vuex-persistedstate";
 
 const store = createStore({
