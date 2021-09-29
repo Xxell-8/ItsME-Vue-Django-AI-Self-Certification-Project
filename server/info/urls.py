c477dd2958bda26fc5651e82af6154e8d1eeef3f
kiung <rldnd22@gmail.com>
2021-09-29 15:09:33 +0900

    [S05P21A204-201] Feat: 만료시간 필터, 링크 삭제 추가

--- server/info/urls.py
+++ server/info/urls.py
@@ -5,7 +5,7 @@ app_name = 'info'
 
 urlpatterns = [
     path('link/', views.link, name='link'),
-    path('link/<int:link_id>/', views.link_detail, name='link_detail'),
-    path('link/<int:link_id>/customer/', views.customer, name='customer'),
+    path('link/<str:link_path>/', views.link_detail, name='link_detail'),
+    path('link/<str:link_path>/customer/', views.customer, name='customer'),
     path('id_card_ocr/', views.id_card_ocr, name='id_card_ocr'),
 ]
