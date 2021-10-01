deab53481a3b2f4cbb0cab8aa8677a4051880b41
ssafy-mann <6513032@gmail.com>
2021-10-01 16:43:47 +0900

    [S05P21A204-184] Design: 모션의 keypoint를 secondary 색상으로 보여주도록 변경

--- client/src/components/customer/verification/utilities.js
+++ client/src/components/customer/verification/utilities.js
@@ -17,7 +17,7 @@
  import * as posenet from "@tensorflow-models/posenet";
  import * as tf from "@tensorflow/tfjs";
  
- const color = "aqua";
+ const color = "#BDFF00";
  const boundingBoxColor = "red";
  const lineWidth = 2;
  
